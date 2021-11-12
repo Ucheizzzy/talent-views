15a50f6c4a7f1bb92de294a7d0f99b35958dd43d
Ayodeji Omoloja <riley@Ayodejis-MacBook-Air.local>
2021-11-12 10:07:26 +0100

    slider -incomplete

--- src/components/list.js
+++ src/components/list.js
@@ -3,36 +3,38 @@ import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icon
 import '../css/list.modules.css'
 import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import HorizontalScroll from 'react-scroll-horizontal'
 
 
 const List = () => {
-    const [slideNumber, setSlideNumber] = useState(0);
-    const [isMoved, setIsMoved] = useState(false);
+    // const [slideNumber, setSlideNumber] = useState(0);
+    // const [isMoved, setIsMoved] = useState(false);
 
     const listRef = useRef()
 
-    const handleClick = (direction) => {
-        setIsMoved(true)
-        let distance = listRef.current.getBoundingClientRect().x - 50
-        if (direction === 'left' &&  slideNumber > 0){
-            setSlideNumber(slideNumber - 1)
-            listRef.current.style.transform = `translateX(${230 + distance}px)`
-        }
-        if (direction === 'right' && slideNumber < 5){
-            setSlideNumber(slideNumber + 1)
-            listRef.current.style.transform = `translateX(${-230 + distance}px)`
-        }
-    }
+    // const handleClick = (direction) => {
+    //     setIsMoved(true)
+    //     let distance = listRef.current.getBoundingClientRect().x - 50
+    //     if (direction === 'left' &&  slideNumber > 0){
+    //         setSlideNumber(slideNumber - 1)
+    //         listRef.current.style.transform = `translateX(${230 + distance}px)`
+    //     }
+    //     if (direction === 'right' && slideNumber < 5){
+    //         setSlideNumber(slideNumber + 1)
+    //         listRef.current.style.transform = `translateX(${-230 + distance}px)`
+    //     }
+    // }
     return (
         <div className='list'>
             <span className="listTitle">Continue to watch</span>
-            <div className="wrapper">
-                <ArrowBackIosOutlined 
+            <div className="wrapper" style={{overflowX: 'visible'}}>
+                {/* <ArrowBackIosOutlined 
                 className='sliderArrow left' 
                 onClick={()=>handleClick('left')}
                 style={{display: !isMoved && 'none'}}
-                />
-                <div className="container" ref={listRef}> 
+                /> */}
+                {/* <HorizontalScroll className="scroll" style={{overflow:'visible', position:'relative'}}> */}
+                <div className="container" ref={listRef} > 
                     <Link to="/home/watch">
                         <ListItem index={0}/>
                     </Link>
@@ -62,9 +64,10 @@ const List = () => {
                     </Link>
                     <Link to="/home/watch">
                         <ListItem index={9}/>
-                    </Link>
+                    </Link>   
                 </div>
-                <ArrowForwardIosOutlined className='sliderArrow right' onClick={()=>handleClick('right')}/>
+                {/* </HorizontalScroll>  */}
+                {/* <ArrowForwardIosOutlined className='sliderArrow right' onClick={()=>handleClick('right')}/> */}
             </div>
         </div>
     )
